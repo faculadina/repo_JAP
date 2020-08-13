6b56aba1f1b885ffb29f70e5efd26b0c7b6adb7e
Facundo Laña <64237137+faculadina@users.noreply.github.com>
2020-08-13 19:00:05 -0300

    Json

--- js/products.js
+++ js/products.js
@@ -15,7 +15,7 @@ function showCategoriesList(array){
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
                         <h4 class="mb-1">`+ category.name +`</h4>
-                        <small class="text-muted">` + category.soldCount + ` artículos</small>
+                        <small class="text-muted">` + category.currency + ": " + category.cost + ` </small>
                     </div>
                     <div>` + category.description + `</div>
 
