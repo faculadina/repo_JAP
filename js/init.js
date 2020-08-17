6ed2ae97b1d8f03ceb839e4bdb8cad298f8b9145
Facundo Laña <64237137+faculadina@users.noreply.github.com>
2020-08-16 21:54:25 -0300

    login no loop

--- js/init.js
+++ js/init.js
@@ -40,9 +40,7 @@ var getJSONData = function(url){
     });
 }
 
-function loginOnce(){
-  location.href="login.html";
-}
+
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
